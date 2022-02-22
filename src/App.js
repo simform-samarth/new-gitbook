052e72922ab68db55e80ed0ed085707a0e713853
Samarth Shah <samarth.s@simformsolutions.com>
2022-02-22 17:15:51 +0530

    changed .yaml path for docs

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React,{useState , useEffect} from 'react'
 import './App.css';
 // import README from ''
 // import Markdown from 'markdown-to-jsx';
+import Markdown from 'markdown-to-jsx';
+// import Page2 from './docs/README.md'
 
 function App() {
 //   let [ content, setContent] = useState({md: ""});
-//   useEffect(()=> {
-//     fetch(README)
-//         .then((res) => res.text())
-//         .then((md) => {
-//             setContent({ md })
-//         })
-// }, [])
+// const file_name = 'react_pinterest_clone.md';
+const [post, setPost] = useState('');
+// useEffect(() => {
+//   import('./docs/README.md')
+//       .then(res => {
+//           fetch(res.default)
+//               .then(res => res.text())
+//               .then(res => setPost(res))
+//               .catch(err => console.log(err));
+//       })
+//       .catch(err => console.log(err));
+// });
+console.log("post",post);
   return (
     <div className="App">
-      {/* <Markdown children={content.md}/> */}
+           <Markdown>
+                {post}
+            </Markdown>
     </div>
   );
 }
